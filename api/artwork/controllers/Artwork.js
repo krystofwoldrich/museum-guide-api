d07d47cfc3ce86991050878ac253215fd1c0aa4c
Kryštof Woldřich <krystof51@gmail.com>
2019-05-02 19:37:55 +0100

    add artwork

---
+++ api/artwork/controllers/Artwork.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Artwork.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Artwork`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve artwork records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx, next, { populate } = {}) => {
+    if (ctx.query._q) {
+      return strapi.services.artwork.search(ctx.query);
+    } else {
+      return strapi.services.artwork.fetchAll(ctx.query, populate);
+    }
+  },
+
+  /**
+   * Retrieve a artwork record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    return strapi.services.artwork.fetch(ctx.params);
+  },
+
+  /**
+   * Count artwork records.
+   *
+   * @return {Number}
+   */
+
+  count: async (ctx, next, { populate } = {}) => {
+    return strapi.services.artwork.count(ctx.query, populate);
+  },
+
+  /**
+   * Create a/an artwork record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.artwork.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an artwork record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.artwork.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an artwork record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.artwork.remove(ctx.params);
+  }
+};
